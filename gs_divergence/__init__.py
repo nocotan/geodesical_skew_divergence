d70eb7d87fc1246b70a81c636cf540b0ae9be08f
Masanari Kimura <noconoco.lib@gmail.com>
2021-04-01 12:22:44 +0900

    upload pypi

--- gs_divergence/__init__.py
+++ gs_divergence/__init__.py
@@ -1,4 +1,4 @@
 from gs_divergence.geodesical_skew_divergence import gs_div
 from gs_divergence.symmetrized_geodesical_skew_divergence import symmetrized_gs_div
 
-__version__ = '0.0.1'
+__version__ = '1.0.0'
